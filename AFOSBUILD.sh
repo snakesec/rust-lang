21de7fbdbaa484d7b69e2455d085329ad59af297
Weidsom Nascimento <source@snakesecurity.org>
2024-04-26 15:50:23 -0300

    Update rust-lang to 1.72.2

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,9 +1,25 @@
 rm -rf /opt/ANDRAX/rust-lang
 
 if [ $(uname -m | grep 'x86_64') ]; then
-   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-toolchain=1.72.0
+   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-toolchain=1.72.2
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Install RUST... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
 else
-   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-host aarch64-unknown-linux-gnu --default-toolchain=1.72.0
+   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-host aarch64-unknown-linux-gnu --default-toolchain=1.72.2
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Install RUST... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
 fi
 
 cp -Rf andraxbin/* /opt/ANDRAX/bin
