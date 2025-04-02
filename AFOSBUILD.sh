e18c4eb2dc9bdf736ebce5071d99de75940fac46
Weidsom Nascimento <source@snakesecurity.org>
2025-04-02 11:14:01 -0700

    Update rust installer to 1.85.1

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,7 +1,7 @@
 rm -rf /opt/ANDRAX/rust-lang
 
 if [ $(uname -m | grep 'x86_64') ]; then
-   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-toolchain=1.80.1
+   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-toolchain=1.85.1
    if [ $? -eq 0 ]
    then
      # Result is OK! Just continue...
@@ -11,7 +11,7 @@ if [ $(uname -m | grep 'x86_64') ]; then
      exit 1
    fi
 else
-   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-host aarch64-unknown-linux-gnu --default-toolchain=1.80.1
+   RUSTUP_INIT_SKIP_PATH_CHECK=yes CARGO_HOME=/opt/ANDRAX/rust-lang RUSTUP_HOME=/opt/ANDRAX/rust-lang sh rustup.sh -y --default-host aarch64-unknown-linux-gnu --default-toolchain=1.85.1
    if [ $? -eq 0 ]
    then
      # Result is OK! Just continue...
